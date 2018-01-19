20eb55345ef2cd0cf01ae6b88895c139c703a0d8
Guest User <Guest@Epicodus-8C.local>
2018-01-19 11:28:56 -0800

    add 0=beep, 1=boop, and input%3===0

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+function beepBoop(userInput) {
+
+
+
+
+    var beep = [0-9]
+    var boop = [0]
+    if (userInput === "1"){
+      return "boop";
+   }  else if (userInput === "0") {
+     return "beep";
+   }   else if (userInput % 3 ===0) {
+        return "Im sorry, Dave I'm afraid I can't do that."
+   }
+ }
+ $(document).ready(function() {
+   $("#userInput").submit(function(event) {
+ var userInput1 = $("#userInput1").val();
+ var newArray = [];
+ for (i=0; i<userInput1.length; i++) {
+   newArray.push(beepBoop(userInput1[i]));
+ }
+ alert(newArray);
+ event.preventDefault();
 
 
 
@@ -5,8 +29,5 @@
 
 
 
-$(document).ready(function() {
-  $("#userInput").submit(function(event) {
-    event.preventDefault();
-  }
-}
+});
+});
