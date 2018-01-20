4f140c9e444427e1a81aa015b2f8cdf87c840fa2
Guest User <Guest@Epicodus-8C.local>
2018-01-19 16:28:27 -0800

    add css and .charAt() attributes

---
+++ README.md
@@ -0,0 +1,39 @@
+# _{Beep Boop Beep}_
+
+#### _{range converter that replaces certain numbers with words}, {01/19/18}_
+
+#### By _**{Hamza Naeem}**_
+
+## Description
+
+_{This is a webpage that counts numbers after receiving an input from the user }_
+
+## Setup/Installation Requirements
+
+* _Open the terminal or Gitbash in you computer
+* _clone the repository from github under hamzilitary username_
+* _Go to the folder named Beep Boop Code Review_
+* _go to the html and js and cs to open all corresponding folders to get full effect_
+* _format_
+
+##Specifications##
+_{It can replace a "1" with a boop}_
+_{it can replace a "0" with a beep}
+
+## Known Bugs
+
+_{have not figured out how to make the input count from 0 up to the inputted number . }_
+
+## Support and contact details
+
+_{if you run into to an issue please email me at hamzilitary22@gmail.com.}_
+
+## Technologies Used
+
+_{this webpage uses HTML, JavaScript, and CSS.}_
+
+### License###
+
+
+
+Copyright (c) 2016 **_{List of contributors or company name}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 function beepBoop(userInput) {
+var beep = [0-9];
+var newNumberString = userInput.toString();
 
+  if (userInput.charAt(0) === "0") {
+    return "beep";
+}   else if (userInput.charAt(0) === "1") {
+    return "boop";
+}     else if (userInput % 3 === 0 && userInput > 0) {
+      return "Im sorry, Dave. Im afraid I cant do that.";
+  }     else if (userInput.charAt(0) % 3 === 0 && userInput > 0) {
+          return "Im sorry, Dave. Im afraid I cant do that.";
+    }       else if (userInput === "0") {
+              return "beep";
+      }         else if (userInput === "1") {
+                  return "boop";
+        }           else {
+                      return userInput;
+        }
+};
 
-
-
-    var beep = [0-9]
-    var boop = [0]
-    if (userInput === "1"){
-      return "boop";
-   }  else if (userInput === "0") {
-     return "beep";
-   }   else if (userInput % 3 ===0) {
-        return "Im sorry, Dave I'm afraid I can't do that."
-   }
- }
- $(document).ready(function() {
+$("#results").hide;
+$(document).ready(function() {
+   var newArray = [];
    $("#userInput").submit(function(event) {
- var userInput1 = $("#userInput1").val();
- var newArray = [];
- for (i=0; i<userInput1.length; i++) {
-   newArray.push(beepBoop(userInput1[i]));
+     var userInput1 = $("#userInput1").val();
+     for (i=0; i<userInput1.length; i++) {
+       newArray.push(beepBoop(userInput1[i]));
  }
- alert(newArray);
- event.preventDefault();
-
-
-
-
-
-
-
+      $("#results").text(newArray.join());
+      event.preventDefault();
 });
 });
